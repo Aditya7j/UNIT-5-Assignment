34cb4897e0e52b18a7e6cafb28215aa001b9312c
Aditya7j <89588001+Aditya7j@users.noreply.github.com>
2022-04-17 20:16:04 +0530

    nav

---
+++ navbar/src/components/Links.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Links = () => {
+  return (
+    <div className='ab'>
+        <span>Service</span>
+        <span>Project</span>
+        <span>About</span>
+    </div>
+  )
+}
+
+export default Links
---
+++ navbar/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div>
+        <img className='logo' src = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Logo-Logo.svg/2560px-Logo-Logo.svg.png" alt="err"/>
+    </div>
+  )
+}
+
+export default Logo
