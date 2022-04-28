3fedfdcb1e3b7f0a0ec281f32743dd7be3eb84f2
Aditya7j <89588001+Aditya7j@users.noreply.github.com>
2022-04-28 20:58:03 +0530

    auth

---
+++ auth-api/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Navbar } from './components/navbar';
+import { Status } from './components/status';
+
+function App() {
+  return (
+  <div className="App">
+   <Navbar/>
+   <Status/>
+    </div>
+  );
+}
+
+export default App;
